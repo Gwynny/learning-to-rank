789c67c74881652d2d2fd3c49773c3bd5b7833ea
Gwynny <yahoo660@gmail.com>
2022-12-05 15:14:10 +0300

    add text preparation stage

--- lesson5_KNRM/knrm_solution.py
+++ lesson5_KNRM/knrm_solution.py
@@ -1,8 +1,10 @@
+import itertools
 import string
 from collections import Counter
 from typing import Dict, List, Tuple, Union, Callable
 
 import nltk
+nltk.download('punkt')
 import numpy as np
 import math
 import pandas as pd
@@ -250,21 +252,34 @@ class Solution:
         })
         return glue_df_fin
 
-    def hadle_punctuation(self, inp_str: str) -> str:
-        # допишите ваш код здесь
-        pass
+    def handle_punctuation(self, inp_str: str) -> str:
+        translator = str.maketrans(string.punctuation, ' '*len(string.punctuation))
+        new_str = inp_str.translate(translator)
+        return new_str
 
     def simple_preproc(self, inp_str: str) -> List[str]:
-        # допишите ваш код здесь
-        pass
+        no_punctuation_str = self.handle_punctuation(inp_str)
+        lowered_str = no_punctuation_str.lower()
+        splitted_doc = nltk.word_tokenize(lowered_str)
+        return splitted_doc
 
     def _filter_rare_words(self, vocab: Dict[str, int], min_occurancies: int) -> Dict[str, int]:
-        # допишите ваш код здесь
-        pass
+        filtered_vocab = {x: count for x, count in vocab.items() if count > min_occurancies}
+        return filtered_vocab
 
     def get_all_tokens(self, list_of_df: List[pd.DataFrame], min_occurancies: int) -> List[str]:
-        # допишите ваш код здесь
-        pass
+        preped_series = []
+        for df in list_of_df:
+            preped_question1 = df['question1'].apply(simple_preproc)
+            preped_question2 = df['question2'].apply(simple_preproc)
+            preped_series.append(preped_question1)
+            preped_series.append(preped_question2)
+
+        concat_series = pd.concat(preped_series)
+        one_list_of_tokens = list(itertools.chain.from_iterable(concat_series.to_list()))
+        vocab = dict(Counter(one_list_of_tokens))
+        vocab = self._filter_rare_words(vocab, min_occurancies)
+        return [key for key, _ in vocab.items()]
 
     def _read_glove_embeddings(self, file_path: str) -> Dict[str, List[str]]:
         # допишите ваш код здесь
